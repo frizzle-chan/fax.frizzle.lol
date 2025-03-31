e68c23c449db6bff41653160dc105d27e0ad0b29
frizzle-chan <frizzle@frizzle.wtf>
2025-03-31 02:25:24 +0000

    linting

--- fax_frizzle/__init__.py
+++ fax_frizzle/__init__.py
@@ -6,15 +6,18 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+
 
 @bot.hybrid_command()
 async def fax(ctx: commands.Context):
     print(ctx.message.author.name)
     print(ctx.message.author.display_avatar)
     await ctx.send("This is a hybrid command!")
+
 
 def run(token: str):
     bot.run(token)
