303b88e213b5445c72ef8f374a4866aa1a9a4370
frizzle-chan <frizzle@frizzle.wtf>
2025-04-06 13:37:58 -0500

    wip

--- fax_frizzle/render/engine.py
+++ fax_frizzle/render/engine.py
@@ -1,10 +1,12 @@
 """
 Image rendering engine for the fax service.
 """
+from typing import Union
 from PIL import Image, ImageDraw, ImageFont
 
+from fax_frizzle.fax import Fax
 
-def render_fax(width: int) -> Image.Image:
+def render_fax(fax: Fax, width: int) -> Image.Image:
     """
     Creates a simple "Hello World" image using PIL.
 
@@ -15,23 +17,40 @@ def render_fax(width: int) -> Image.Image:
         PIL Image object with "Hello World" text
     """
     # Create a new image with white background
-    size = (800, 300)
+    size = (width, width)
     img = Image.new('RGB', size, color=(255, 255, 255))
     draw = ImageDraw.Draw(img)
+
+    gr = 1.618  # Golden ratio
+
+    h_margin = int(width // gr // (2*3))
+    v_margin = int(width // gr // (2*3))
+    scroll = 0
 
     # Use default font
     font = ImageFont.load_default()
 
-    # Get the text size
-    text = "Hello World"
-    text_width, text_height = (
-        draw.textbbox((0, 0), text, font=font)[2:4]  # For newer PIL versions
-    )
+    def draw_centered_text(text: str, font: Union[ImageFont.FreeTypeFont, ImageFont.ImageFont], **kwargs) -> None:
+        text_width, text_height = (
+            draw.textbbox((0, 0), text, font=font)[2:4]
+        )
+        defaults = {
+            "fill": (0, 0, 0),
+        }
+        # Merge defaults with any kwargs provided
+        final_kwargs = {**defaults, **kwargs}
+        draw.text(
+            ((width - text_width) / 2, scroll),
+            text,
+            **final_kwargs
+        )
 
-    # Calculate position to center the text
-    position = ((size[0] - text_width) // 2, (size[1] - text_height) // 2)
+    scroll += v_margin
+    fax.user_avatar.thumbnail((150, 150))
+    img.paste(fax.user_avatar, (int(width // 2) - int(fax.user_avatar.width // 2), scroll))
+    scroll += fax.user_avatar.height
+    scroll += int(v_margin // 2)
 
-    # Draw the text in black
-    draw.text(position, text, font=font, fill=(0, 0, 0))
+    draw_centered_text(font=font, text=fax.user_name)
 
     return img
--- fax_frizzle/render/engine_test.py
+++ fax_frizzle/render/engine_test.py
@@ -1,11 +1,23 @@
 from io import BytesIO
+from PIL import Image
+import arrow
 
+from fax_frizzle.fax import Fax
 from fax_frizzle.render.engine import render_fax
+import os
+from pathlib import Path
 
+# Get the directory where the current file is located
+current_dir = Path(os.path.dirname(os.path.abspath(__file__)))
 
 def test_render_fax(image_regression):
     # Create the image
-    img = render_fax()
+    img = render_fax(width=512,
+                      fax=Fax(user_name="Test User",
+                              user_avatar=Image.open(current_dir / "../img/question.png"),
+                              text="Hello World",
+                              ts=arrow.get('2013-05-11T21:23:58.970460+07:00').datetime,
+                              image_attachments=[]))
     stream = BytesIO()
     img.save(stream, "PNG")
     image_regression.check(stream.getvalue())
