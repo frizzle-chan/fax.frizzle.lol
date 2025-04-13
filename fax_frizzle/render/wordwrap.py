784457db47037cf8151dc66efa6f598f794bb6e3
frizzle-chan <frizzle@frizzle.wtf>
2025-04-12 21:07:56 -0500

    More efficient wordwrap algorithm
    Before: O(k) calls to font.getlength() where k is the total number of
    non whitespace tokens
    
    After:
    - Short lines get one call to font.getlength()
    - Long lines get O(k)+1 calls to font.getlength() where k is the number of
      non whitespace tokens in the long line

--- fax_frizzle/render/wordwrap.py
+++ fax_frizzle/render/wordwrap.py
@@ -1,31 +1,33 @@
 
 import re
-from collections import deque
 from typing import List
 
 from PIL import ImageFont
 
 
 def wordwrap(font: ImageFont.FreeTypeFont, width: int, text: str) -> str:
-    tokens = deque(re.split(r'(\s+)', text))
-    lines = []
-    current_line: List[str] = []
-    while tokens:
-        token = tokens.popleft()
-        # Handle whitespace token
-        if not token.strip():
-            for ws in re.split(r'(\n)', token):
-                if ws == "\n":
-                    lines.append(''.join(current_line))
-                    current_line = []
-                elif ws:
-                    current_line.append(ws)
+    raw_lines = text.splitlines()
+    out_lines: List[str] = []
+    for raw_line in raw_lines:
+        if font.getlength(raw_line) <= width:
+            out_lines.append(raw_line)
             continue
-        # Check if the line fits in the width
-        if font.getlength(''.join(current_line + [token])) <= width:
+
+        current_line: List[str] = []
+        token: str
+        for token in re.split(r'(\s+)', raw_line):
             current_line.append(token)
-            continue
-        # If it doesn't fit, add the current line to the lines list and start a new line
-        lines.append(''.join(current_line))
-        current_line = [token]
-    return '\n'.join(lines)
+            # Handle whitespace token
+            if not token.strip():
+                continue
+            # Check if the line fits in the width
+            if font.getlength(''.join(current_line)) <= width:
+                continue
+            # If it doesn't fit, add the current line to the lines list and start a new line
+            current_line.pop()
+            out_lines.append(''.join(current_line))
+            current_line = [token]
+
+        if current_line:
+            out_lines.append(''.join(current_line))
+    return '\n'.join(out_lines)
